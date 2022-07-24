71dbf2a70c13c65c8e8e557669673ead0ca45d99
Dor Fire <dorfire@gmail.com>
2022-07-24 10:37:55 +0300

    Fix build

--- src/onlyany/onlyany.go
+++ src/onlyany/onlyany.go
@@ -18,8 +18,8 @@ var Analyzer = &analysis.Analyzer{
 
 func run(pass *analysis.Pass) (res any, err error) {
 	astInspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	astInspector.Preorder([]ast.Node{(*ast.Expr)(nil)}, func(node ast.Node) {
-		switch e := n.(type) {
+	astInspector.Preorder([]ast.Node{(ast.Expr)(nil)}, func(node ast.Node) {
+		switch e := node.(type) {
 		case *ast.InterfaceType:
 			if len(e.Methods.List) == 0 {
 				pass.Reportf(e.Pos(), "use any instead of an empty interface")
